064a20847421437611ac6d793029abe940aff6f3
Francesco4203 <nnlong21@apcs.fitus.edu.vn>
2022-11-22 12:33:27 +0700

    cvehicle function implemented

--- CS202 - Project/CVEHICLE.cpp
+++ CS202 - Project/CVEHICLE.cpp
@@ -9,30 +9,43 @@
 #include "CTRAFFIC.h"
 using namespace std;
 using namespace sf;
-void CVEHICLE::Move() {
 
-}
 void CVEHICLE::stop() {
-
+	isStop = true;
 }
 void CVEHICLE::resume() {
-
+	isStop = false;
 }
-CVEHICLE::CVEHICLE() {
-
+Sprite CVEHICLE::getObject() {
+	return object;
 }
-CVEHICLE::CVEHICLE(int mode) {
-
+CVEHICLE::CVEHICLE(int x, int y, int mode) {
+	speed = (mode == 1 ? 5 : (mode == 2 ? 7 : 10));
+	isStop = 1;
+	object.setPosition(x, y);
+	object.scale(0.3, 0.3);
 }
-CTRUCK::CTRUCK(int mode) : CVEHICLE(mode) {
-
+CTRUCK::CTRUCK(int x, int y, int mode) : CVEHICLE(x, y, mode) {
+	vehicle.loadFromFile("Resource/truck.png");
+	object.setTexture(vehicle);
 }
 CTRUCK::~CTRUCK() {
 
 }
-CCAR::CCAR(int mode) : CVEHICLE(mode) {
-
+void CTRUCK::Move() {
+	if (isStop) return;
+	srand(time(NULL));
+	object.move((rand() % speed + 3) / 10.0, 0);
+}
+CCAR::CCAR(int x, int y, int mode) : CVEHICLE(x, y, mode) {
+	vehicle.loadFromFile("Resource/car.png");
+	object.setTexture(vehicle);
 }
 CCAR::~CCAR() {
 
+}
+void CCAR::Move() {
+	if (isStop) return;
+	srand(time(NULL));
+	object.move((rand() % (speed + 5) + 3) / 10.0, 0);
 }
